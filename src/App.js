60bb5242fc4fde73011f00e1106ba75a58bfa0e8
Trung Kien <kientuoi94@gmail.com>
2021-07-25 00:26:04 +0700

    add isSignedIn and check it in onRouteChange, add route "register"/"signin"/"signout"

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
   calculateFaceLocation = (data) => {
@@ -62,23 +63,29 @@ class App extends Component {
       .catch(err => console.log(err))
   }
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
     this.setState({ route: route });
   }
   render() {
+    const { isSignedIn, box, imageUrl, route } = this.state;
     return (
       <div className="App">
         <Particles className="particles"
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange} />
-        {this.state.route === 'home'
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home'
           ? <>
             <Logo />
             <Rank />
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </>
-          : (this.state.route === 'signin')
+          : (route === 'signin')
             ? <SignIn onRouteChange={this.onRouteChange} />
             : <Register onRouteChange={this.onRouteChange} />
         }
